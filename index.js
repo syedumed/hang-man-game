b040d1c5de917ba0939a380ff22479eca6d296ec
syed umed asim <umed7095@gmail.com>
2024-09-22 19:05:44 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,287 @@
+const animals = [
+    "Aardvark",
+    "Albatross",
+    "Alligator",
+    "Alpaca",
+    "Ant",
+    "Anteater",
+    "Antelope",
+    "Ape",
+    "Armadillo",
+    "Donkey",
+    "Baboon",
+    "Badger",
+    "Barracuda",
+    "Bat",
+    "Bear",
+    "Beaver",
+    "Bee",
+    "Bison",
+    "Boar",
+    "Buffalo",
+    "Butterfly",
+    "Camel",
+    "Capybara",
+    "Caribou",
+    "Cassowary",
+    "Cat",
+    "Caterpillar",
+    "Cattle",
+    "Chamois",
+    "Cheetah",
+    "Chicken",
+    "Chimpanzee",
+    "Chinchilla",
+    "Chough",
+    "Clam",
+    "Cobra",
+    "Cockroach",
+    "Cod",
+    "Cormorant",
+    "Coyote",
+    "Crab",
+    "Crane",
+    "Crocodile",
+    "Crow",
+    "Curlew",
+    "Deer",
+    "Dinosaur",
+    "Dog",
+    "Dogfish",
+    "Dolphin",
+    "Dotterel",
+    "Dove",
+    "Dragonfly",
+    "Duck",
+    "Dugong",
+    "Dunlin",
+    "Eagle",
+    "Echidna",
+    "Eel",
+    "Eland",
+    "Elephant",
+    "Elk",
+    "Emu",
+    "Falcon",
+    "Ferret",
+    "Finch",
+    "Fish",
+    "Flamingo",
+    "Fly",
+    "Fox",
+    "Frog",
+    "Gaur",
+    "Gazelle",
+    "Gerbil",
+    "Giraffe",
+    "Gnat",
+    "Gnu",
+    "Goat",
+    "Goldfinch",
+    "Goldfish",
+    "Goose",
+    "Gorilla",
+    "Goshawk",
+    "Grasshopper",
+    "Grouse",
+    "Guanaco",
+    "Gull",
+    "Hamster",
+    "Hare",
+    "Hawk",
+    "Hedgehog",
+    "Heron",
+    "Herring",
+    "Hippopotamus",
+    "Hornet",
+    "Horse",
+    "Human",
+    "Hummingbird",
+    "Hyena",
+    "Ibex",
+    "Ibis",
+    "Jackal",
+    "Jaguar",
+    "Jay",
+    "Jellyfish",
+    "Kangaroo",
+    "Kingfisher",
+    "Koala",
+    "Kookabura",
+    "Kouprey",
+    "Kudu",
+    "Lapwing",
+    "Lark",
+    "Lemur",
+    "Leopard",
+    "Lion",
+    "Llama",
+    "Lobster",
+    "Locust",
+    "Loris",
+    "Louse",
+    "Lyrebird",
+    "Magpie",
+    "Mallard",
+    "Manatee",
+    "Mandrill",
+    "Mantis",
+    "Marten",
+    "Meerkat",
+    "Mink",
+    "Mole",
+    "Mongoose",
+    "Monkey",
+    "Moose",
+    "Mosquito",
+    "Mouse",
+    "Mule",
+    "Narwhal",
+    "Newt",
+    "Nightingale",
+    "Octopus",
+    "Okapi",
+    "Opossum",
+    "Oryx",
+    "Ostrich",
+    "Otter",
+    "Owl",
+    "Oyster",
+    "Panther",
+    "Parrot",
+    "Partridge",
+    "Peafowl",
+    "Pelican",
+    "Penguin",
+    "Pheasant",
+    "Pig",
+    "Pigeon",
+    "Pony",
+    "Porcupine",
+    "Porpoise",
+    "Quail",
+    "Quelea",
+    "Quetzal",
+    "Rabbit",
+    "Raccoon",
+    "Rail",
+    "Ram",
+    "Rat",
+    "Raven",
+    "Red deer",
+    "Red panda",
+    "Reindeer",
+    "Rhinoceros",
+    "Rook",
+    "Salamander",
+    "Salmon",
+    "Sand Dollar",
+    "Sandpiper",
+    "Sardine",
+    "Scorpion",
+    "Seahorse",
+    "Seal",
+    "Shark",
+    "Sheep",
+    "Shrew",
+    "Skunk",
+    "Snail",
+    "Snake",
+    "Sparrow",
+    "Spider",
+    "Spoonbill",
+    "Squid",
+    "Squirrel",
+    "Starling",
+    "Stingray",
+    "Stinkbug",
+    "Stork",
+    "Swallow",
+    "Swan",
+    "Tapir",
+    "Tarsier",
+    "Termite",
+    "Tiger",
+    "Toad",
+    "Trout",
+    "Turkey",
+    "Turtle",
+    "Viper",
+    "Vulture",
+    "Wallaby",
+    "Walrus",
+    "Wasp",
+    "Weasel",
+    "Whale",
+    "Wildcat",
+    "Wolf",
+    "Wolverine",
+    "Wombat",
+    "Woodcock",
+    "Woodpecker",
+    "Worm",
+    "Wren",
+    "Yak",
+    "Zebra"
+]
+
+
+let lives = 6;
+
+// Getting a random animal from the list
+let number = Math.floor(Math.random() * animals.length);
+let randomAnimal = animals[number].toLowerCase(); // Convert the random animal to lowercase
+console.log(randomAnimal);
+
+// Variable to store the dashed version of the animal
+let dashedAnimal = Array(randomAnimal.length).fill("-");
+
+// Button to start the game and display dashed letters
+let start = document.querySelector(".start_button");
+start.addEventListener('click', () => {
+    document.querySelector(".dashed-letters").textContent = dashedAnimal.join('');
+});
+
+// Getting user's input (letter buttons)
+var numberOfButtons = document.querySelectorAll(".letter").length;
+
+for (var i = 0; i < numberOfButtons; i++) {
+    document.querySelectorAll(".letter")[i].addEventListener("click", function () {
+        var guessedLetter = this.innerHTML.toLowerCase();
+        console.log(guessedLetter);
+
+        let correctGuess = false;
+
+        // Check if the guessed letter is in the random animal
+        for (let j = 0; j < randomAnimal.length; j++) {
+            if (guessedLetter === randomAnimal[j]) {
+                dashedAnimal[j] = randomAnimal[j]; // Update the dashed word with the correct letter
+                correctGuess = true;
+            }
+        }
+
+        // Update the dashed word on the screen
+        document.querySelector(".dashed-letters").textContent = dashedAnimal.join('');
+
+        // Check if the whole word has been guessed
+        if (dashedAnimal.join('') === randomAnimal) {
+            document.querySelector(".win-or-lose").style.display = "block";
+            document.querySelector(".letters").style.display = "none";
+            document.querySelector(".dashed-letters").style.display = "none";
+            document.querySelector(".win-or-lose").textContent = "Congratulations";
+        } else if (!correctGuess) {
+            lives -= 1;
+            if(lives === 0){
+                document.querySelector(".win-or-lose").style.display = "block";
+                document.querySelector(".letters").style.display = "none";
+                document.querySelector(".dashed-letters").style.display = "none";
+                document.querySelector(".win-or-lose").textContent = "Sorry You Lost, Try again "; 
+            }
+        }
+        console.log(lives)
+        document.querySelector("img").src = "./Hangman images/hangman"+lives+".png"
+        document.querySelector(".restart_button").addEventListener('click',() => {
+            location.reload();
+        });
+    });
+}
